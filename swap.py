6f893314a3389980266bb9bd59445a7d2b141262
Prathamesh199 <75716229+Prathamesh199@users.noreply.github.com>
2021-02-08 10:58:10 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,14 @@
+string=input("Enter string:")
+count1=0
+count2=0
+for i in string:
+      if(i.islower()):
+            count1=count1+1
+      elif(i.isupper()):
+            count2=count2+1
+            
+print("The number of lowercase")
+print(count1)
+print("The number of uppercase")
+print("Swap Characters")
+print(string.swapcase())
